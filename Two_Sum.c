4dcd2183e26a372771e919c205f66b03ebd1621f
yd7w <yahyaboukhmira7@gmail.com>
2025-02-05 16:22:30 +0100

    1 problem Done

--- Two_Sum.c
+++ Two_Sum.c
@@ -14,30 +14,30 @@ int*	twoSum(int* nums, int numsSize, int target, int* returnSize)
 	}
 	*returnSize = 2;
 	result = (int *)malloc((*returnSize) * sizeof(int));
-    if (!result){
-        *returnSize = 0;
-        return (NULL);
-    }
-    while (j != numsSize)
-    {
-        while (i != numsSize)
-        {
-            if (i == j){
-                i++;
-                if (i == numsSize || j == numsSize)
-                    break;
-            }
-            if (nums[i] + nums[j] == target)
-            {
-              result[0] = j;
-              result[1]= i;
-              return (result);
-            }
-            i++;
-        }
-        i = 0;
-        j++;
-    }
-    *returnSize = 0;
-    return (NULL);
+	if (!result){
+		*returnSize = 0;
+		return (NULL);
+	}
+	while (j != numsSize)
+	{
+		while (i != numsSize)
+		{
+			if (i == j){
+            	i++;
+            	if (i == numsSize || j == numsSize)
+                	break;
+			}
+			if (nums[i] + nums[j] == target)
+			{
+				result[0] = j;
+            	result[1]= i;
+            	return (result);
+			}
+			i++;
+		}
+		i = 0;
+		j++;
+	}
+	*returnSize = 0;
+	return (NULL);
 }
